27b97e79c44a31e2a10282cadbcbde1c5139633c
Huy Doan <huy11900@gmail.com>
2022-02-20 17:13:22 +0700

    complete header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.scss'
 
 export default function Header() {
     return (
-        <Navbar expand="lg" bg="white" variant="light" className="p-3 shadow-sm">
+        <Navbar expand="lg" bg="white" className="p-3 shadow-sm">
             <Container>
                 <Navbar.Brand href="/" className="text-uppercase fw-bold">EduZone</Navbar.Brand>
                 <Nav className="mx-auto d-none d-lg-flex">
@@ -20,7 +20,7 @@ export default function Header() {
                 </Nav>
                 <Nav className="d-none d-lg-flex">
                     <Nav.Link as={NavLink} to="/signUp">Sign Up</Nav.Link>
-                    <Nav.Link as={NavLink} to="/signUp" className="btn-invert px-3 py-2 align-self-center">Sign In</Nav.Link>
+                    <Nav.Link as={NavLink} to="/signUp" className="btn-invert px-4 py-2 align-self-center">Sign In</Nav.Link>
                 </Nav>
                 <Navbar.Toggle aria-controls="offcanvasNavbar" />
                 <Navbar.Offcanvas
@@ -34,10 +34,10 @@ export default function Header() {
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
                             <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                            {/* <Nav.Divider /> */}
+                            <NavDropdown.Divider />
                             <Nav.Link as={NavLink} to="/courses">All Course</Nav.Link>
-                            {/* <Nav.Divider /> */}
-                            <NavDropdown title="Pages" id="collasible-nav-dropdown">
+                            <NavDropdown.Divider />
+                            <NavDropdown title="Pages" id="collasible-nav-dropdown" className='rounded-0'>
                                 <NavDropdown.Item as={NavLink} to="/1">Action</NavDropdown.Item>
                                 <NavDropdown.Item as={NavLink} to="/2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item as={NavLink} to="/3">Something</NavDropdown.Item>
@@ -45,15 +45,10 @@ export default function Header() {
                                 <NavDropdown.Item as={NavLink} to="/4">Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
-                        <Form className="d-flex">
-                            <FormControl
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        <Nav className="d-flex flex-row justify-content-evenly m-3">
+                            <Nav.Link as={NavLink} to="/signUp">Sign Up</Nav.Link>
+                            <Nav.Link as={NavLink} to="/signUp" className="btn-invert px-3 py-2 align-self-center">Sign In</Nav.Link>
+                        </Nav>
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Container>
