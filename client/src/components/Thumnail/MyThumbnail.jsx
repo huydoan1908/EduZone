1c5918317a356a22ef0da7ded96572374aff7431
Huy Doan <huy11900@gmail.com>
2022-02-21 20:24:06 +0700

    My course list

---
+++ client/src/components/Thumnail/MyThumbnail.jsx
@@ -0,0 +1,34 @@
+import { ProgressBar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './MyThumbnail.scss';
+export default function Thumbnail() {
+    return (
+        <div className="thumbnail">
+            <Link to="#">
+                <div className="thumbnail-img">
+                    <img
+                        src="https://template.hasthemes.com/edule/eduLe/assets/images/courses/courses-01.jpg"
+                        alt="Thumbnail Image"
+                    />
+                </div>
+            </Link>
+            <div className="thumbnail-content">
+                <div className="teacher-info">
+                    <div className="avatar">
+                        <Link to="#">
+                            <img
+                                src="https://template.hasthemes.com/edule/eduLe/assets/images/author/author-01.jpg"
+                                alt="Teacher Avatar"
+                                className="rounded-circle"
+                            />
+                        </Link>
+                    </div>
+                    <Link to="#">Jason William</Link>
+                </div>
+                <Link to="#"><h5>Data Science and Machine Learning with Python - Hands On!</h5></Link>
+                <p className="my-3">38% Complete</p>
+                <ProgressBar now={38} variant="success" />
+            </div>
+        </div>
+    )
+}
