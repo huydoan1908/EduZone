e84cd42440d1c359368f1fce99516c18c1956f02
tranmanhduc2711 <manhduc2711@gmail.com>
2022-03-02 14:59:05 +0700

    update login/signup

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './Login.scss'
-import {Button,FormControl,InputGroup} from 'react-bootstrap'
+import {Container,Row,Col,Button,FormControl,InputGroup} from 'react-bootstrap'
 export default function Login() {
   return (
     <>
-      <div className="login-wrapper">
+      <Container className="login-container">
+        <Row>
+          <Col className="login-image">
+            <img src="https://template.hasthemes.com/edule/eduLe/assets/images/register-login.png"></img>
+          </Col>
+          <Col style={{marginTop:"80px",paddingLeft: "3rem"}}>
+            <div className="login-form">
+              <div className="title">
+                Login
+                <span> Now</span>
+              </div>
+              <div className="form">
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Username or Email"
+                    style={{ margin: "2rem 0 1rem 0", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Password"
+                    style={{ marginBottom: "1rem", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <div className="login-submit">
+                  <Button
+                    variant="primary"
+                    style={{ margin: "1rem 0 1rem 0", borderRadius: "1rem" }}
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    variant="primary"
+                    style={{
+                      borderRadius: "1rem",
+                    }}
+                  >
+                    Login with Google
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      {/* <div className="login-wrapper">
         <div className="login-image">
           <img src="https://template.hasthemes.com/edule/eduLe/assets/images/register-login.png"></img>
         </div>
@@ -49,7 +98,7 @@ export default function Login() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,71 +1,75 @@
 import React from 'react'
-import {InputGroup,FormControl,Button} from 'react-bootstrap'
+import {InputGroup,FormControl,Button,Container,Row,Col} from 'react-bootstrap'
 import './SignUp.scss'
 export default function SignUp() {
   return (
     <>
-      <div className="signup-wrapper">
-        <div className="signup-image">
-          <img src="https://template.hasthemes.com/edule/eduLe/assets/images/register-login.png"></img>
-        </div>
+      <Container className="signup-container">
+        <Row>
+          <Col className="signup-image">
+            <img src="https://template.hasthemes.com/edule/eduLe/assets/images/register-login.png"></img>
+          </Col>
 
-        <div className="signup-form">
-          <div className="title">
-            Registration
-            <span> Now</span>
-          </div>
-          <div className="form">
-            <InputGroup size="lg">
-              <FormControl
-                aria-label="Large"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Name"
-                style={{ margin: "2rem 0 1rem 0", borderRadius: "1rem" }}
-              />
-            </InputGroup>
-            <InputGroup size="lg">
-              <FormControl
-                aria-label="Large"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Email"
-                style={{ marginBottom: "1rem", borderRadius: "1rem" }}
-              />
-            </InputGroup>
-            <InputGroup size="lg">
-              <FormControl
-                aria-label="Large"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Password"
-                style={{ marginBottom: "1rem", borderRadius: "1rem" }}
-              />
-            </InputGroup>
-            <InputGroup size="lg">
-              <FormControl
-                aria-label="Large"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Confirm Password"
-                style={{ marginBottom: "1rem", borderRadius: "1rem" }}
-              />
-            </InputGroup>
-            <div className="login-submit">
-              <Button
-                variant="primary"
-                style={{ margin: "1rem 0 1rem 0", borderRadius: "1rem" }}
-              >
-                Login
-              </Button>
-              <Button
-                variant="primary"
-                style={{
-                  borderRadius: "1rem",
-                }}
-              >
-                Login with Google
-              </Button>
+          <Col className="col-signup">
+            <div className="signup-form">
+              <div className="title">
+                Registration
+                <span> Now</span>
+              </div>
+              <div className="form">
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Name"
+                    style={{ margin: "2rem 0 1rem 0", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Email"
+                    style={{ marginBottom: "1rem", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Password"
+                    style={{ marginBottom: "1rem", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <InputGroup size="lg">
+                  <FormControl
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="Confirm Password"
+                    style={{ marginBottom: "1rem", borderRadius: "1rem" }}
+                  />
+                </InputGroup>
+                <div className="signup-submit">
+                  <Button
+                    variant="primary"
+                    style={{ margin: "1rem 0 1rem 0", borderRadius: "1rem" }}
+                  >
+                    SIGNUP
+                  </Button>
+                  <Button
+                    variant="primary"
+                    style={{
+                      borderRadius: "1rem",
+                    }}
+                  >
+                    SIGNUP with Google
+                  </Button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
